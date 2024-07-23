e31e18be6308e1f7490c2f5170bb79cf888459e0
gargaditya748 <132154451+gargaditya748@users.noreply.github.com>
2024-07-23 21:44:39 +0530

    Add files via upload

---
+++ gemini_ai/main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  2 20:55:45 2024
+
+@author: DELL
+"""
+
+import os
+
+from PIL import Image
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+st.set_page_config(
+    page_title="Gemini AI",
+    page_icon="🧠",
+    layout="centered",
+)
+
+with st.sidebar:
+    selected = option_menu('Gemini AI',
+                           ['ChatBot',
+                            'Image Captioning',
+                            'Embed text',
+                            'Ask me anything'],
+                           menu_icon='robot', icons=['chat-dots-fill', 'image-fill', 'textarea-t', 'patch-question-fill'],
+                           default_index=0
+                           )
